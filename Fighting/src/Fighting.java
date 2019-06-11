2f8a4ccc448f4e50c171391143b6d0a064a3d4fc
shivamgupta57121 <shivamgupta57121@gmail.com>
2019-06-11 18:45:55 +0530

    Fight until death

--- Fighting/src/Fighting.java
+++ Fighting/src/Fighting.java
@@ -24,40 +24,53 @@ public class Fighting {
 		int yourDamage = scan.nextInt();
 		System.out.print("Your Life Points : ");
 		int yourLife = scan.nextInt();
+		System.out.println();
 		
+		scan.nextLine();
 		Random generator = new Random();
-		boolean attacker = generator.nextBoolean();
-		if(attacker) {
-			System.out.println("Your attack");
-			int dice = generator.nextInt(6)+1 + generator.nextInt(6)+1;
-			int attackValue = yourAttack + dice;
-			System.out.println("Rolled Value : "+dice);
-			System.out.println("Your attack value : "+attackValue);
-			if (attackValue > monsterDefence) {
-				System.out.println("Your attack was successful. ");
-				monsterLife = monsterLife - yourDamage;
-				System.out.println("Monster's remaining Life : "+monsterLife);
+		do {
+			boolean attacker = generator.nextBoolean();
+			if(attacker) {
+				System.out.println("Your attack");
+				int dice = generator.nextInt(6)+1 + generator.nextInt(6)+1;
+				int attackValue = yourAttack + dice;
+				System.out.println("Rolled Value : "+dice);
+				System.out.println("Your attack value : "+attackValue);
+				if (attackValue > monsterDefence) {
+					System.out.println("Your attack was successful. ");
+					monsterLife = monsterLife - yourDamage;
+					System.out.println("Monster's remaining Life : "+monsterLife);
+				}
+				else {
+					System.out.println("Your attack was not successful. ");
+				}
+				
 			}
 			else {
-				System.out.println("Your attack was not successful. ");
+				System.out.println("Monster attacks. ");
+				int dice = generator.nextInt(6)+1 + generator.nextInt(6)+1;
+				int attackValue = monsterAttack + dice;
+				System.out.println("Rolled Value : "+dice);
+				System.out.println("Monster's attack value : "+attackValue);
+				if (attackValue > yourDefence) {
+					System.out.println("Monster's attack was successful. ");
+					yourLife = yourLife - monsterDamage;
+					System.out.println("Your remaining Life : "+yourLife);
+				}
+				else {
+					System.out.println("Monster's attack was not successful. ");
+				}
 			}
 			
-		}
-		else {
-			System.out.println("Monster attacks. ");
-			int dice = generator.nextInt(6)+1 + generator.nextInt(6)+1;
-			int attackValue = monsterAttack + dice;
-			System.out.println("Rolled Value : "+dice);
-			System.out.println("Monster's attack value : "+attackValue);
-			if (attackValue > yourDefence) {
-				System.out.println("Monster's attack was successful. ");
-				yourLife = yourLife - monsterDamage;
-				System.out.println("Your remaining Life : "+yourLife);
-			}
-			else {
-				System.out.println("Monster's attack was not successful. ");
-			}
-		}
+			System.out.println("Press Enter to continue. ");
+			scan.nextLine();
+		}while(yourLife>0 && monsterLife>0);
+		
+		if(monsterLife>0)
+			System.out.println("You are dead. :-( ");
+		else
+			System.out.println("You won. The monster is dead. :-) ");
+		
 
 	}
 
